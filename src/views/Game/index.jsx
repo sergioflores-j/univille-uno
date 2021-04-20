3fd0fd2f0ba70b34d3c37eff5869f26a3cc7fa9f
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2021-04-19 22:26:20 -0300

    feat: initial board state (players cards)

--- src/views/Game/index.jsx
+++ src/views/Game/index.jsx
@@ -1,16 +1,110 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
 import Board from 'components/Board';
 import { usePlayer } from 'context/player';
-import React from 'react';
+import { generateCardsPile } from 'functions/pile';
+import { shuffle } from 'utils/array';
+
+const generatedPile = generateCardsPile();
+const [firstCard, initialBotCards, initialPlayerCards, rest] = [
+  generatedPile[0],
+  generatedPile.slice(1, 8),
+  generatedPile.slice(8, 14),
+  generatedPile.slice(15),
+];
 
 const Game = () => {
   const player = usePlayer();
+  const mounted = useRef();
+  const [pile, setPile] = useState(rest);
+  const [discardedPile, setDiscardedPile] = useState([firstCard]);
+  const [players, setPlayers] = useState([
+    {
+      id: 1,
+      name: 'Crazy frog (bot)',
+      isHuman: false,
+      cards: initialBotCards,
+    },
+    {
+      id: 2,
+      name: player.name,
+      isHuman: true,
+      cards: initialPlayerCards,
+    },
+  ]);
 
-  console.log('player', player);
+  const pileSelfHeal = useCallback(() => {
+    if (pile.length === 0) {
+      setPile(shuffle(discardedPile));
+      setDiscardedPile([]);
+    }
+  }, [pile, discardedPile]);
+
+  const setPlayerCards = (playerIndex, cards) =>
+    setPlayers(old => {
+      const copy = [...old];
+      copy[playerIndex] = { ...copy[playerIndex], cards };
+
+      return copy;
+    });
+
+  const drawCardsFromPile = useCallback(
+    (quantity = 1) => {
+      if (!pile.length) return [];
+
+      const drawedCards = pile.slice(0, quantity);
+
+      setPile(old => [...old.slice(quantity - 1)]);
+
+      return drawedCards;
+    },
+    [pile]
+  );
+
+  const drawCards = useCallback(
+    (playerIndex, quantity = 1) => {
+      if (!players[playerIndex]) return;
+
+      const { cards } = players[playerIndex];
+
+      setPlayerCards(playerIndex, [...cards, drawCardsFromPile(quantity)]);
+    },
+    [drawCardsFromPile, players]
+  );
+
+  useEffect(() => {
+    if (!mounted.current) return;
+
+    pileSelfHeal();
+  }, [pileSelfHeal]);
+
+  useEffect(() => {
+    if (mounted.current) return;
+
+    setPlayers(old => {
+      const copy = [...old];
+      copy[1].name = player.name;
+
+      return [...copy];
+    });
+  }, [player]);
+
+  useEffect(() => {
+    if (mounted.current) return;
+
+    mounted.current = true;
+  }, []);
+
   return (
     <>
       <h1>Game</h1>
 
-      <Board />
+      <Board
+        pile={pile}
+        discardedPile={discardedPile}
+        players={players}
+        onDrawCards={drawCards}
+      />
     </>
   );
 };
