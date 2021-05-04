13a23d5fa8a146bd12f57435e4d94a8d83ee5b73
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2021-05-04 20:58:14 -0300

    feat: find playable card with ranking

--- src/utils/game.js
+++ src/utils/game.js
@@ -9,9 +9,56 @@ export const isPlayableCard = (lastPlayedCard, card) => {
   return false;
 };
 
-// TODO: find the best available card? (most with same color/number?)
+export const getCardRanking = (lastPlayedCard, cards, card) => {
+  let ranking = 0;
+
+  if (!isPlayableCard(lastPlayedCard, card)) return ranking;
+
+  if (lastPlayedCard.color === card.color) ranking += 1;
+  if (lastPlayedCard.card === card.card) ranking += 1;
+
+  const cardsByColor = new Map();
+  const cardsByCardNumber = new Map();
+
+  cards.forEach(c => {
+    cardsByColor.set(c.color, [...(cardsByColor.get(c.color) || []), c]);
+    cardsByCardNumber.set(c.card, [
+      ...(cardsByCardNumber.get(c.card) || []),
+      c,
+    ]);
+  });
+
+  if (
+    cardsByColor.get(card.color)?.length &&
+    !Array.from(cardsByColor.values()).find(
+      val => val.length > cardsByColor.get(card.color)?.length
+    )
+  )
+    ranking += 1;
+
+  if (
+    cardsByCardNumber.get(card.card)?.length &&
+    !Array.from(cardsByCardNumber.values()).find(
+      val => val.length > cardsByColor.get(card.card)?.length
+    )
+  )
+    ranking += 1;
+
+  return ranking;
+};
+
 export const findPlayableCard = (lastPlayedCard, cards) =>
-  cards.find(c => isPlayableCard(lastPlayedCard, c));
+  [...cards].reduce((acc, c) => {
+    const ranking = getCardRanking(lastPlayedCard, cards, c);
+
+    if (ranking === 0) return acc;
+
+    if (ranking > (acc?.ranking || 0)) {
+      return { ...c, ranking };
+    }
+
+    return acc;
+  }, null);
 
 export const delay = (timeout = 1000) =>
   new Promise(resolve => setTimeout(resolve, timeout));
